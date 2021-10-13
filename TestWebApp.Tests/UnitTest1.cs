4716ec8c91fdf04789a5b0c16c7a0aa36cf9560f
prashanth9292 <prasantpurimitla@gmail.com>
2021-10-13 22:31:30 +0530

    first commit

---
+++ TestWebApp.Tests/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestWebApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+        }
+    }
+}
